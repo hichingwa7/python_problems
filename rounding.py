588da1985d16197c60e5b941312d1176ca382700
Humphrey I Shikoli <hshikoli@unr.edu>
2019-09-05 22:04:42 -0700

    round and format methods

---
+++ rounding.py
@@ -0,0 +1,14 @@
+# List numbers from 0 to 10, divide each by 3,
+# print result as 2 decimal places
+
+# round()
+#format()
+
+divisor = 3
+decimal_places = 2
+
+for number in range(0,11):
+    print("{0:.2f}".format(number/divisor))
+   
+    
+
