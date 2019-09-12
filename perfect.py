d52aa8c473b781704e6014a316a380084eae2d19
hichingwa7 <hichingwa7@gmail.com>
2019-09-12 15:42:04 -0700

    three digit perfect number

--- perfect.py
+++ perfect.py
@@ -9,13 +9,14 @@
 import sys
 
 if len(sys.argv) != 2:
-    print("Error: pass one integer as the only argument")
+    print("Error: pass one positive integer as the only argument")
     sys.exit()
     
 arg_number = int(sys.argv[1])
 lower_bound = 99
 upper_bound = 999
 isitperfect = 0
+divisor = []
 
 #############################################################################
 # check whether the number entered is a 3-digit number
@@ -23,13 +24,15 @@ isitperfect = 0
 # check whether the number suffices to be a perfect no.
 
 if arg_number < lower_bound or arg_number == lower_bound or arg_number > upper_bound:
-    print("Error: Please input a three digit number")
+    print("Error: Please input a three digit positive integer")
 elif arg_number > lower_bound and arg_number < upper_bound or arg_number == upper_bound:
     for i in range(1, arg_number):
         if arg_number % i == 0:
-            divisor = i
-            isitperfect += divisor
+            divisor.append(i)
+    isitperfect = sum(divisor)
+    
     if isitperfect == arg_number:
-        print(str(isitperfect) + " is a 3-digit perfect number")    
+        print(*divisor, sep = '+', end = "")
+        print(" = " + str(isitperfect))    
     else:
         print("The 3-digit number entered is not a perfect number")
