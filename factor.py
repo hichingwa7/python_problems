7054b400a24dbde13c26b6e046d4f8558c7b10f9
hichingwa7 <hichingwa7@gmail.com>
2019-09-12 04:22:04 -0700

    program prints multiples of 5, 7 or 13

--- factor.py
+++ factor.py
@@ -3,6 +3,8 @@
 # programming language: Python
 # description: program that prints multiples of 5, 7 or 13 between 0 and 101
 
+###########################################################################################
+
 factor5 = 5
 factor7 = 7
 factor13 = 13
@@ -12,6 +14,7 @@ upper_bound = 101
 ############################################################################################
 # for loop iterates over the numbers between 0 to 101
 # if statements check whether current no. under iteration range is a multiple of 5,7 or 13
+
 for x in range(lower_bound,upper_bound):
     if x % factor5 == 0 and x % factor7 == 0 and x % factor13 == 0 and x != 0:
         print(str(x) + " is a multiple of " + str(factor5) + " and " + str(factor7) + " and " + str(factor13) + "!")
