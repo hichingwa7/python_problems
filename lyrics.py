c53d21e1edf0983c3f1e8bb0d1fd11b002b39a2f
hichingwa7 <hichingwa7@gmail.com>
2019-10-12 23:22:02 -0700

    reading files and manipulating the dictionary

--- lyrics.py
+++ lyrics.py
@@ -5,30 +5,60 @@
 # dictionary. Filenames are provided in the command line as arguments
 
 import sys
-data_dict = {}
-data_dict_2 = {}
-print(sys.argv[1:])
+import json
 
-for songfile in sys.argv[1:]:
-    title = ""
-    with open(songfile) as infile:
-        songtext = infile.readlines()
-        thisisthefirstline = True
-        for line in songtext:
-            if thisisthefirstline:
-                thisisthefirstline = False
-                title = line
-                continue
-            words = line.split()
-            for w in words:
-                if w in data_dict:
-                    data_dict[w] += 1
-                else:
-                    data_dict[w] = 1
-                data_dict_2[w] = {'count':[w], 'songs':[title]}
-        print(data_dict_2)
-     
+file_list = []
+words = []
+unique_words = []
+file_dict = {}
+final_dict = {}
 
+textdict = []
+
+argvlen = len(sys.argv) - 1
+if argvlen < 2:
+    print("Pass atleast one textfile at the command line argument")
+
+position = 1
+try:
+    while position <= argvlen:
+        print(argvlen)
+        with open(sys.argv[position], 'r') as infile:
+            textdict.append(sys.argv[position])
+        position += 1
+except IndexError:
+    sys.exit
+
+for each_file in textdict:
     
-                    
-                
+    with open(each_file, "r+") as infile:
+        file_data = infile.readlines()
+        thisisthefirstline = True
+        endoffile = False
+    for line in file_data:
+        if thisisthefirstline:
+            thisisthefirstline = False
+            continue
+        file_data_1 = line.split(" ")
+        for n in file_data_1:
+            
+            words.append(n)
+
+for w in words:
+    if w not in unique_words:
+        unique_words.append(w)
+
+for each_word in unique_words:
+    count = words.count(each_word)
+    song_list = []
+    for each_file in textdict:
+        with open(each_file, "r+") as current_file:
+            file_data_1 = current_file.read().split()
+            if each_word in file_data_1:
+                song_list.append(each_file)
+
+    final_dict.update({each_word.lower(): {"Count": count, "Songs": song_list}})
+
+print(final_dict)
+with open("lyrics_json_1_songs.txt", 'w') as outfile:
+    json.dump(final_dict,outfile, indent = 4)
